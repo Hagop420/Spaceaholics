1f9ec9faf92fd7353eb9c113068805e2f6ae77f5
Hagop Almadjian <hagop1isaboss@hotmail.com>
2023-11-29 06:06:03 +0000

    Trying-to-parse-planet-image-inDOM

--- js/main.js
+++ js/main.js
@@ -14,15 +14,26 @@ const apodImgcopyrightClaims = document.querySelector(
   '.space-APOD-content > p.copyright',
 );
 
+const planetImagesCall = document.querySelector('.AJAX-planet-image');
+
+// classes for the form element
+// // Font awesome magnifying glass
+const magnifying_glass = document.querySelector('.fa-magnifying-glass');
+
+// if user types in a planet name get the correct planet image
+const inputSearch = document.querySelector('form input').value;
+
+magnifying_glass.classList.add('fa-magnifying-glass');
+magnifying_glass.classList.add('fa-solid');
+magnifying_glass.classList.add('m-1');
+magnifying_glass.classList.add('bg-info');
+magnifying_glass.classList.add('rounded');
+
 // APOD Main Img AJAX Call
 function apodImgMain() {
   // API request creation with XMLHTTPrequest
   const xhr = new XMLHttpRequest();
-  xhr.open(
-    'GET',
-    `https://api.nasa.gov/planetary/apod?api_key=${apiKey}`,
-    true,
-  );
+  xhr.open('GET', `https://api.nasa.gov/planetary/apod?api_key=${apiKey}`);
   xhr.responseType = 'json';
   xhr.addEventListener('load', () => {
     const createImgTag = document.createElement('img');
@@ -39,11 +50,7 @@ apodImgMain();
 function apodImgExplanation() {
   // API request creation with XMLHTTPrequest
   const xhr = new XMLHttpRequest();
-  xhr.open(
-    'GET',
-    `https://api.nasa.gov/planetary/apod?api_key=${apiKey}`,
-    true,
-  );
+  xhr.open('GET', `https://api.nasa.gov/planetary/apod?api_key=${apiKey}`);
   xhr.responseType = 'json';
   xhr.addEventListener('load', () => {
     // Creating h2 element for the APOD image
@@ -69,3 +76,27 @@ function apodImgExplanation() {
 }
 
 apodImgExplanation();
+
+// Calling the Image/Video API
+
+function userTypeInput(e) {
+  // API request creation with XMLHTTPrequest
+  const xhr = new XMLHttpRequest();
+  xhr.open('GET', `https://images-api.nasa.gov/search?q=pluto`);
+  xhr.responseType = 'json';
+
+  xhr.addEventListener('load', () => {
+    console.log(xhr.response.collection);
+  });
+
+  function renderPlanetImages() {
+    const imgElement = document.createElement('img');
+    igmElement.setAttribute('src');
+  }
+
+  magnifying_glass.addEventListener('click', () => {
+    xhr.send();
+  });
+}
+
+userTypeInput();
